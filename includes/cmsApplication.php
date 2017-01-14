fab83c601f66c5768c698eb80f67aa991b74b53c
L200130174 <fredyka.maulana@gmail.com>
2017-01-14 09:21:41 +0700

    perbaikan pertama cms

---
+++ includes/cmsApplication.php
@@ -0,0 +1,5 @@
+<?php
+require_once('cmsBase.php');
+class cmsApplication extends cmsBase{
+	
+}
---
+++ includes/templateFunctions.php
@@ -0,0 +1,5 @@
+<?php
+require_once('cmsBase.php');     
+class templateFunctions extends cmsBase{         
+  //Semua fungsi yang terkait dengen pengaturan template ada di sini.
+}
